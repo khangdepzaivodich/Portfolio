8bb5a5730ae92f6d4b6f0c50c4fe747b85797748
Phan Tuan Khang <phantuankhanghcmue@gmail.com>
2024-06-12 21:37:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
+
+  const removeActiveClasses = () => {
+    navLinks.forEach((link) => {
+      link.classList.remove("text-yellow-400");
+    });
+  };
+
+  const addActiveClass = (id) => {
+    const activeLink = document.querySelector(`nav a[href="#${id}"]`);
+    if (activeLink) {
+      activeLink.classList.add("text-yellow-400");
+    }
+  };
+
+  const observerCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(`${entry.target.id} is intersecting`);
+        removeActiveClasses();
+        addActiveClass(entry.target.id);
+      }
+    });
+  };
+
+  const observerOptions = {
+    root: null,
+    threshold: 0.5,
+  };
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+  // Observe each section
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+
+  // Click event for logo
+  const logo = document.getElementById("logo");
+  logo.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    removeActiveClasses();
+    const homeLink = document.querySelector(`nav a[href="#home"]`);
+    if (homeLink) {
+      homeLink.classList.add("text-yellow-400");
+    }
+  });
+  document.addEventListener("scroll", () => {
+    const scrollArrow = document.getElementById("scroll-arrow");
+    if (window.scrollY > 0) {
+      scrollArrow.style.opacity = 0;
+    } else {
+      scrollArrow.style.opacity = 100;
+    }
+  });
+});
